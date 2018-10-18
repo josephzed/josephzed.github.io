8ae36e546a2ea56748becea66d16f1e824f73fde
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2018-10-18 01:08:14 -0700

    Added watch product

--- js/product.visibility.js
+++ js/product.visibility.js
@@ -1,2 +1,3 @@
+document.getElementById('product-retro-watch').style.display = 'visible'
 document.getElementById('product-tshirt').style.display = 'none'
 document.getElementById('product-yoyo').style.display = 'visible'
