7e4dd95a07bc1ea17d1f71bcf0bb9139d3bd0159
Daniel Weisberg <DWEISBER@uwaterloo.ca>
2018-10-18 21:39:12 -0700

    Added additional products

--- js/product.visibility.js
+++ js/product.visibility.js
@@ -1,3 +1,8 @@
 document.getElementById('product-retro-watch').style.display = 'visible'
-document.getElementById('product-tshirt').style.display = 'none'
-document.getElementById('product-yoyo').style.display = 'visible'
+document.getElementById('product-necklace').style.display = 'visible'
+document.getElementById('product-fashion-watch').style.display = 'visible'
+document.getElementById('product-eyelashes').style.display = 'visible'
+document.getElementById('product-protector').style.display = 'visible'
+document.getElementById('product-earphones').style.display = 'visible'
+
+//document.getElementById('product-tshirt').style.display = 'none'
